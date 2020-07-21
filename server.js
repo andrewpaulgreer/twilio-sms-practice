40c1570ccbd86fd407953840245aed29eb9d8e3c
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-07-21 13:56:30 -0400

    playing around with twilio sms

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+    to: '8287738001',
+    from: '+1 866 939 1612',
+    body: "testing for twillio message"
+}).then((message)=> console.log(message))
+.catch((err) => console.log(err))
